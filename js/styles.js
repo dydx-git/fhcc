c4baddbcdc2b0ccbf0502c5bf2424d3f0ecea5fa
Muhammad Taha <al.dighton@outlook.com>
2019-06-10 19:37:29 +0500

    Adding bulma-slider

---
+++ js/styles.js
@@ -0,0 +1,1 @@
+// document.getElementById("add-doctor").style.display="none";
--- public/views/doctors.php
+++ public/views/doctors.php
@@ -6,3 +6,13 @@ require_once __DIR__ . '/../../twig.php';
 
 echo $twig->render('doctors/list.twig', [
                     'doctors' => $doctors]);
+echo $twig->render('doctors/add.twig');
+
+echo '        </div>
+    </div>
+
+<script src="/fhcc/node_modules/bulma-slider/dist/js/bulma-slider.min.js"></script>
+<script src="/fhcc/js/styles.js"></script>
+</body>
+
+</html>';
